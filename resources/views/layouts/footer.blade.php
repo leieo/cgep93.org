172b913534becc63efc65007948953be7086432d
textSolver34761 <pradonbenjamin@gmail.com>
2018-03-22 13:55:15 +0100

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
 <footer class="blog-footer">
-  ceci est un footer
+<h3>Nous contacter:</h3> <br>
+  Nous sommes au : <br>
+  Le siège est <br>
+  Vous avez une question ? Nous sommes joingable au 01.47.00.47.20 ou par mail contact@cgep93.org. <!--==> formulaire de contact--> <br>
+  pour toutes demande de recherches, veuillez utlisez le formulaire.  <!-- Mettre FontAwsome Maison, Téléphone, etc. -->
 </footer>
