14c84aa5d1144301f0bdbd08786d086e9b965660
textSolver34761 <pradonbenjamin@gmail.com>
2018-03-23 16:14:11 +0100

    V1 footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,35 +1,34 @@
-<br>
-<br>
-<address class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-link fas fa-home">
-          Cercle Généalogie de l'Est Parisien<br>
-          115 route de soie<br>
-          Bâtiment du savoir<br>
-          75008 Paris<br>
-          France
-        </li>
-        <li class="nav-link fas fa-home">
-          Le siège du Cercle Généalogie de l'Est Parisien<br>
-          1000 route du Président Roosvelt<br>
-          Bâtiment du Ministre de la Culture<br>
-          75012 Paris<br>
-          France
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Nous contacter:
-            <a class="dropdown-item fas fa-at navbar-dark bg-dark" href="{{ Route('contact') }}">Envoyez nous un mail</a>
-            <a class="dropdown-item fas fa-phone navbar-dark bg-dark">Appellez-nous au 00.00.00.00.00</a>
-            <a class="dropdown-item fas fa-search-plus navbar-dark bg-dark" href="{{ Route('formsearch') }}">Vos demandes de recherches</a>
-          </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link fab fa-facebook-f" href="https://www.facebook.com" target="_blank">Facebook</a>
-        </li>
-      </ul>
+<footer class="bg-dark mt-5 p-5 text-white">
+  <div class="row">
+    <div class="col-md-4 col-sm-12">
+      <p>L'adresse de l'association</p>
+      <p>
+        Cercle Généalogie de l'Est Parisien<br>
+        115 route de soie<br>
+        Bâtiment du savoir<br>
+        75008 Paris<br>
+        France
+      </p>
     </div>
-  </nav>
-</address>
-&copy; Le Cercle Généalogique de l'Est Parisien - <?php echo date("Y");?>, tous droits réservés
+    <div class="col-md-4 col-sm-12">
+      <p>L'adresse du siège</p>
+      <p>
+        Le siège du Cercle Généalogie de l'Est Parisien<br>
+        1000 route du Président Roosvelt<br>
+        Bâtiment du Ministre de la Culture<br>
+        75012 Paris<br>
+        France
+      </p>
+    </div>
+    <div class="col-md-4 col-sm-12">
+      <p>Nous retrouver</p>
+      <a class="nav-link pl-0 pt-0 fas fa-at text-white" href="mailto:contact@cgep93.org">Envoyez nous un mail</a>
+      <a class="nav-link pl-0 fas fa-phone text-white">Appellez-nous au 00.00.00.00.00</a>
+      <a class="nav-link pl-0 fas fa-search-plus text-white" href="{{ Route('formsearch') }}">Vos demandes de recherches</a>
+      <a class="nav-link pl-0 fab fa-facebook-f text-white" href="https://www.facebook.com" target="_blank">Facebook</a>
+    </div>
+    <div class="col-md-12 mt-5 text-center">
+      &copy; Le Cercle Généalogique de l'Est Parisien - <?php echo date("Y");?>, tous droits réservés
+    </div>
+  </div>
+</footer>
