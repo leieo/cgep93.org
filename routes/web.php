a3c2cf00ebcdba72631ad9a346720adb7d357101
ldhy <laura.delahaye@gmail.com>
2018-04-09 16:24:37 +0200

    le stash de la mort 2

--- routes/web.php
+++ routes/web.php
@@ -21,41 +21,6 @@ Route::get('/', 'HomeController@index');
     Route::get('/association', 'AssociationController@association')->name('association');
 
 // Vie Associative Route - Longue page avec ancres et renvoi pour chaque section
-<<<<<<< Updated upstream
-    Route::get('/communitylife', 'CommunitylifeController@communitylife')->name('communitylife'); //Vie associative
-    Route::get('/bulletins'); //Bulletins
-    Route::get('/summary');//Listes sommaires
-    Route::get('/paperstatements');//Relevés papiers
-    Route::get('/genecole');//Génécole
-    Route::get('/events');//Evènements
-
-// Recherches
-    Route::get('/search', 'SearchController@search')->name('search');//Recherche
-    Route::get('/geneabank');//Généabank
-    Route::get('/bigenet');//Bigenet
-    Route::get('/tips');//Trucs et astuces
-    Route::get('/department');//Département
-    Route::get('/archives');//Archives
-    Route::get('/links');//Liens
-    Route::get('/worldresearch');//Recherches dans le monde
-    Route::get('/library');//Photothèque
-
-
- // Authentication Routes...
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-    // Registration Routes...
-    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-    Route::post('register', 'Auth\RegisterController@register');
-
-    // Password Reset Routes...
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-=======
 Route::get('/communitylife', 'CommunitylifeController@communitylife')->name('communitylife'); //Vie associative
 Route::get('/bulletins', 'CommunitylifeController@bulletins')->name('bulletins');  //Bulletins
 Route::get('/summary', 'CommunitylifeController@summary')->name('summary');//Listes sommaires
@@ -90,7 +55,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
->>>>>>> Stashed changes
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
