5c221ef70fca599ad828817c5c2ecdd38bda3ee7
leieo <leila.el.omari@gmail.com>
2018-04-11 16:28:09 +0200

    css page association + Membres CA

--- resources/views/association.blade.php
+++ resources/views/association.blade.php
@@ -4,14 +4,82 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 col-md-offset-2">
-          <h2>L'association </h2>
-<p>Fondée en juin 1987 (Journal officiel n°31 du 5 août 1987). </p>
-<p>Régie par la loi du 1er juillet 1901 (statuts déposés à la Préfecture de la Seine-Saint-Denis sous le numéro 87-5373).</p>
-<p>Affiliée à l'Union Généalogique Francilienne.</p>
+            <h2>Statuts</h2>
+			<p>Fondée en juin 1987 (Journal officiel n°31 du 5 août 1987). </p>
+			<p>Régie par la loi du 1er juillet 1901 (statuts déposés à la Préfecture de la Seine-Saint-Denis sous le numéro 87-5373).</p>
+			<p>Affiliée à l'Union Généalogique Francilienne.</p>
+			<p>Déclarée à la <a href="https://www.cnil.fr/" target="_blank">CNIL</a> le 16 février 1998 sous le n° 547999</p>
+			<p>L'assemblée générale du 2 mars 2002 a voté pour le changement du nom de l'association qui devient le "Cercle de Généalogie et d'histoire de la Seine-Saint-Denis". Cette modification est portée au règlement intérieur, mais l'appellation CGEP reste inscrite dans les statuts. </p>             
+			<h2>Membres du CA</h2>      
+			<table align="center"> 
+				<tr>
+					<td>Président</td>
+					<td>Sébastien LEQUEUX</td>
+				</tr>
+				<tr>
+					<td>Vice-président</td>
+					<td>Thierry AUBIE</td>
+				</tr>
+				<tr>
+					<td>Secrétaire</td>
+					<td>Marie LEQUEUX</td>
+				</tr>
+				<tr>
+					<td>Trésorier</td>
+					<td>Pierre CHAZAL</td>
+				</tr>
+				<tr>
+					<td>Trésorier adjoint</td>
+					<td>Hervé SALMON</td>
+				</tr>
+				<tr>
+					<td>Responsable Bibliothèque</td>
+					<td>Geneviève CAMUS</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Responsables Informatique,</p>
+						<p>Correspondants GénéaBank</p>
+					</td>
+					<td>
+						<p>Annick BERTHO</p>
+						<p>Jean-Yves HOUARD</p>
+						<p>Jean LECUYER</p>
+						<p>Valérie POULOULAT</p>
+					</td>
+				</tr>
+			</table>
 
-<p>Déclarée à la CNIL le 16 février 1998 sous le n° 547999</p>
+			<table align="center">
+			<h5>MEMBRES D'HONNEUR</h5>
+				<tr>
+					<td>
+						<p>Présidente fondatrice,</p>
+						<p>Présidente d'honneur</p>
+					</td>
+					<td>Madame Pierrette BRUGERE-PENSIER</td>
+				</tr>
+				<tr>
+					<td>Président d'honneur</td>
+					<td>Monsieur † André CAMUS</td>
+				</tr>
+				<tr>
+					<td>Vice-Président d'honneur</td>
+					<td>Monsieur Claude THIOLET</td>
+				</tr>
+			</table>
+			<table align="center">
+				<tr><td>Madame la directrice des Archives Départementales de la Seine-Saint-Denis</td></tr>
+				<tr><td>Madame le Maire de Noisy-le-Sec</td></tr>
+				<tr><td>Monsieur Jean-Claude TESTART</td></tr>
+				<tr><td>Monsieur Gilbert LECOUTEUX</td></tr>
+				<tr><td>Monsieur Jacques TOUPIN</td></tr>
+			</table>
 
-<p>L'assemblée générale du 2 mars 2002 a voté pour le changement du nom de l'association qui devient le "Cercle de Généalogie et d'histoire de la Seine-Saint-Denis". Cette modification est portée au règlement intérieur, mais l'appellation CGEP reste inscrite dans les statuts. </p> 
+			<h2>Règlement intérieur</h2>         
+			<h2>Infos Pratiques</h2>         
+			<h2>Adhésions</h2>         
+			<h2>Nous contacter</h2>
         </div>
     </div>
 </div>
