6096cd838e2dfe032dba07e6a03a6fc60803f12f
MargotDem <margot_demaulmont@hotmail.fr>
2018-04-11 11:42:48 +0200

    style fixes

--- public/js/map.js
+++ public/js/map.js
@@ -4,35 +4,35 @@ var links = map.querySelectorAll('.map__list a')
 
 // Polyfill du foreach
 if (NodeList.prototype.forEach === undefined) {
-    NodeList.prototype.forEach = function (callback) {
-        [].forEach.call(this, callback)
-    }
+  NodeList.prototype.forEach = function (callback) {
+    [].forEach.call(this, callback)
+  }
 }
 
 var activeArea = function (id) {
-    map.querySelectorAll('.is-active').forEach(function (item) {
-        item.classList.remove('is-active')
-    })
-    if (id !== undefined) {
-        document.querySelector('#list-' + id).classList.add('is-active')
-        document.querySelector('#ville-' + id).classList.add('is-active')
-    }
+  map.querySelectorAll('.is-active').forEach(function (item) {
+    item.classList.remove('is-active')
+  })
+  if (id !== undefined) {
+    document.querySelector('#list-' + id).classList.add('is-active')
+    document.querySelector('#ville-' + id).classList.add('is-active')
+  }
 }
 
 paths.forEach(function (path) {
-    path.addEventListener('mouseenter', function () {
-        var id = this.id.replace('ville-','')
-        activeArea(id)
-    })
+  path.addEventListener('mouseenter', function () {
+    var id = this.id.replace('ville-', '')
+    activeArea(id)
+  })
 })
 
 links.forEach(function (link) {
-    link.addEventListener('mouseenter', function () {
-        var id = this.id.replace('list-', '')
-        activeArea(id)
-    })
+  link.addEventListener('mouseenter', function () {
+    var id = this.id.replace('list-', '')
+    activeArea(id)
+  })
 })
 
 map.addEventListener('mouseover', function () {
-    activeArea()
+  activeArea()
 })
