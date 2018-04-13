a718486c05a0feac4093502b8995c2a82aec86c3
ldhy <laura.delahaye@gmail.com>
2018-04-13 15:55:49 +0200

    fichiers css

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -5,7 +5,64 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h1>Liens</h1>
-        </div>
+              <h2>Pages personelles de nos membres</h2>
+                <p class="intro">Si vous êtes membre du CGEP, vous pouvez :
+                faire référencer ici votre site si vous en avez un,
+                sinon, vous pouvez nous envoyer un fichier Gedcom que nous publierons.
+                Pour celà, envoyez un mail à contact@cgep93.org<p>
+
+              <h2>André Camus</h2>
+                <h3>Pour découvrir la généalogie de notre regretté Président</h3>
+                  <p><a href="http://www.cgep93.org/camus/index.htm" target="_blank">www.cgep93.org/camus/index.html</a></p>
+                  <p><a href="mailto:genevieve.camus@bbox.fr">email : genevieve.camus@bbox.fr</a></p>
+
+              <h2>René Connat	Histoire de Bondy</h2>
+                <h3>La page des CONNAT et CHARTON</h3>
+                  <h4>Cousins : Didier Fontaine et Yves Vergez à Rosny-sous-Bois</h4>
+                    <p><a href="http://bondy-histoire.chez-alice.fr/" target="_blank">http://bondy-histoire.chez-alice.fr/</a></p>
+                    <p><a href="mailto:rconnat@numericable.com">email : rconnat@numericable.com</a></p>
+
+              <h2>Didier Fontaine</h2>
+                <h3>La page des CONNAT et CHARTON</h3>
+                  <h4>Cousins: René Connat et Yves Vergez à Rosny-sous-Bois</h4>
+                    <p><a href="http://pagesperso-orange.fr/tedebe/" target="_blank">http://pagesperso-orange.fr/tedebe/</a></p>
+                    <p><a href="mailto:didier.fontaine@wanadoo.fr">email : didier.fontaine@wanadoo.fr</a></p>
+
+              <h2>Jean Lecuyer</h2>
+                <h3>La page des CONNAT et CHARTON</h3>
+                  <h4>Recherches essentiellement sur les Lécuyer du 93 et plus précisément sur Stains et alentours</h4>
+                    <p>13 générations en Seine-Saint-Denis</p>
+                    <p><a href="mailto:lecuyer2@free.fr">email : lecuyer2@free.fr</a></p>
+
+              <h2>Georges Marquenet</h2>
+                <h3>Généalogie des Marquenet</h3>
+                  <p><a href="http://www.cgep93.org/marquenet/index.php" target="_blank">http://www.cgep93.org/marquenet/index.php</a><br/>
+                  <p><a href="mailto:marquenet.georges@free.fr">email : marquenet.georges@free.fr</a></p>
+
+              <h2>Jean-Claude Melot</h2>
+                <h3>Généalogie personnelle et relevés de Bobigny</h3>
+                  <p><a href="http://jcmelot.free.fr" target="_blank">http://jcmelot.free.fr</a></p>
+                  <p><a href="http://jcmelot.free.fr" target="_blank">http://jcmelot.free.fr</a></p>
+                  <p><a href="mailto:jcmelot@free.fr">email : jcmelot@free.fr</a></p>
+
+              <h2>Gilles Souchet</h2>
+                <h3>Généalogie des Souchet</h3>
+                  <p><a href="http://www.cgep93.org/souchetg/index.php" target="_blank">http://www.cgep93.org/souchetg/index.php</a></p>
+                  <p><a href="http://geneweb.geneanet.org/gsouchet" target="_blank">http://geneweb.geneanet.org/gsouchet</a></p>
+                  <p><a href="mailto:famille.gsouchet@libertysurf.fr">email : famille.gsouchet@libertysurf.fr</a></p>
+
+              <h2>Claude Thiolet</h2>
+                <h3>Généalogie des Thiolet</h3>
+                  <p><a href="http://www.cgep93.org/thiolet/index.php" target="_blank">http://www.cgep93.org/thiolet/index.php</a></p>
+                  <p><a href="mailto:claude.thiolet@wanadoo.fr">email : claude.thiolet@wanadoo.fr</a></p>
+
+              <h2>Yves Vergez</h2>
+                <h3>Généalogie des Vergez, des Vitry et des Mainguet</h3>
+                  <h4>Cousins: R.Connat et Didier Fontaine à Rosny-sous-Bois</h4>
+                    <p><a href="http://www.vergez.net" target="_blank">http://www.vergez.net</a></p>
+                    <p><a href="http://gw.geneanet.org/vergezyv" target="_blank">http://gw.geneanet.org/vergezyv</a></p>
+                    <p><a href="mailto:yves@vergez.net">email : yves@vergez.net</a></p>
+
     </div>
 </div>
 @endsection
