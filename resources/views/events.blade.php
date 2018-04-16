bd8e8f1469e1ecc1f63f9f1f88058eb0a5641977
leieo <leila.el.omari@gmail.com>
2018-04-16 17:36:36 +0200

    réparation fichier manquant

---
+++ resources/views/events.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+
+            <iframe src="https://calendar.google.com/calendar/embed?src=cgep93web%40gmail.com&ctz=Europe%2FParis" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+    </div>
+</div>
