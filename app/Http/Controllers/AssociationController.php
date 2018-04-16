8675b0e1568a2c73bce05c4ff21420b587820068
leieo <leila.el.omari@gmail.com>
2018-04-16 14:52:27 +0200

    supression doublons

--- app/Http/Controllers/AssociationController.php
+++ app/Http/Controllers/AssociationController.php
@@ -10,24 +10,4 @@ class AssociationController extends Controller
   {
       return view('association');
   }
-  public function membresCA()
-  {
-    return view('membresCA');
-  }
-  public function règlement()
-  {
-    return view('règlement');
-  }
-  public function adhésions()
-  {
-    return view('adhésions');
-  }
-  public function infospratiques()
-  {
-    return view('infospratiques');
-  }
-  public function nouscontacter()
-  {
-    return view('nouscontacter');
-  }
 }
